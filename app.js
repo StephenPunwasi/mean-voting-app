d49f3ccbf598673248874ff76fed1225b2d4de09
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-11-04 15:55:40 -0500

    addPost function added

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ angular.module('flapperNews', [])
 '$scope',
 function($scope){
   $scope.posts = [
-  'post 1',
-  'post 2',
-  'post 3',
-  'post 4',
-  'post 5'
+  {title: 'post 1', upvotes: 5},
+  {title: 'post 2', upvotes: 2},
+  {title: 'post 3', upvotes: 15},
+  {title: 'post 4', upvotes: 9},
+  {title: 'post 5', upvotes: 4}
   ];
+
+  $scope.addPost = function(){
+    $scope.posts.push({title: $scope.title, upvotes:0});
+    $scope.title = '';
+  };
 }]);
